232ea6259c5372e552b1625ada0aef38e80c7baf
Muhammad Iqbal <iqbal@indesc.com>
2025-04-19 14:26:41 +0700

    cash flow done

--- app/(main)/flow/[slug]/[id]/page.tsx
+++ app/(main)/flow/[slug]/[id]/page.tsx
@@ -11,11 +11,15 @@ import { last, sortBy } from 'lodash';
 import { useRouter } from 'next/navigation';
 import { FilterMatchMode } from 'primereact/api';
 import { Button } from 'primereact/button';
+import { Calendar } from 'primereact/calendar';
 import { Chart } from 'primereact/chart';
 import { Column } from 'primereact/column';
 import { DataTable, DataTableFilterMeta } from 'primereact/datatable';
+import { InputNumber } from 'primereact/inputnumber';
+import { InputSwitch } from 'primereact/inputswitch';
 import { InputText } from 'primereact/inputtext';
-import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import { Toast } from 'primereact/toast';
+import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 
 dayjs.locale('id');
 dayjs.extend(customParseFormat);
@@ -31,8 +35,16 @@ const TableTransactionRecord = ({ params }: { params: Promise<{ slug: string; id
     const [globalFilterValue, setGlobalFilterValue] = useState('');
     const [category, setCategory] = useState('');
     const [finalCount, setFinalCount] = useState<number>(0);
+    const [isShowForm, setShowForm] = useState(false);
+    const [isSubmitLoad, setSubmitLoad] = useState(false);
+    const [isRemoval, setRemoval] = useState(false);
+    const [payloadAction, setPayloadAction] = useState('baru');
+    const [payloadType, setPayloadType] = useState('');
+    const [payloadAmount, setPayloadAmount] = useState<number | undefined>();
+    const [payloadDate, setPayloadDate] = useState<any>(dayjs().toDate());
 
     const router = useRouter();
+    const toast = useRef<Toast>(null);
     const allowedCategories = useMemo(() => ['saving', 'loan'], []);
     const infoCategories = useMemo<{ [key: string]: string }>(() => ({ saving: 'Simpanan', loan: 'Pinjaman' }), []);
     const mapTypes = useMemo(
@@ -51,7 +63,25 @@ const TableTransactionRecord = ({ params }: { params: Promise<{ slug: string; id
     const typeBodyTemplate = (rowData: ICash) => mapTypes.find(({ value }) => value === rowData.type)?.label || rowData.type;
     const amountBodyTemplate = (rowData: ICash) => formatRp(rowData.amount);
     const countBodyTemplate = (rowData: ICash & { count: number }) => formatRp(rowData.count);
-    const editBodyTemplate = (rowData: ICash) => <Button icon="pi pi-credit-card" outlined onClick={() => router.push(`/account/${rowData.id}`)} />;
+    const editBodyTemplate = (rowData: ICash) => <Button icon="pi pi-pencil" outlined onClick={() => toggleForm(rowData?.type, rowData?.id)} />;
+
+    const toggleForm = (type: string = '', id: string = 'baru') => {
+        setShowForm(!isShowForm);
+        setPayloadAction(id);
+        setPayloadType(type);
+        setPayloadAmount(undefined);
+        setPayloadDate(dayjs().toDate());
+        setRemoval(false);
+
+        if (id !== 'baru') {
+            const exist = list.find((item) => item.id === id);
+
+            if (exist) {
+                setPayloadAmount(exist.amount);
+                setPayloadDate(dayjs(exist.date, 'DD-MM-YYYY').toDate());
+            }
+        }
+    };
 
     const fetchTransactions = useCallback(
         async (category: string, account: string, documentStyle: CSSStyleDeclaration) => {
@@ -169,14 +199,12 @@ const TableTransactionRecord = ({ params }: { params: Promise<{ slug: string; id
             <div className="col-12 lg:col-6">
                 <div className="p-3 h-full">
                     <div className="shadow-2 p-3 h-full flex flex-column" style={{ borderRadius: '6px' }}>
-                        <div className="text-900 font-medium text-xl mb-2 uppercase">{infoCategories[category]}</div>
-                        <div className="text-600">
-                            {category === 'saving' ? 'Saldo Simpanan' : 'Sisa Cicilan'} <span className={`text-${category === 'saving' ? 'green' : 'orange'}-500 font-bold`}>{formatRp(finalCount)}</span>
-                        </div>
-                        <hr className="my-3 mx-0 border-top-1 border-bottom-none border-300" />
                         <div className="flex align-items-center">
                             <span className="font-bold text-2xl text-900">{customer?.name}</span>
                         </div>
+                        <hr className="my-3 mx-0 border-top-1 border-bottom-none border-300" />
+                        <div className="text-600">{category === 'saving' ? 'Saldo Simpanan' : 'Sisa Cicilan'}</div>
+                        <div className={`text-${category === 'saving' ? 'green' : 'orange'}-500 font-bold text-xl mb-2 uppercase`}>{formatRp(finalCount)}</div>
                         <hr className="my-3 mx-0 border-top-1 border-bottom-none border-300" />
                         <ul className="list-none p-0 m-0 flex-grow-1">
                             <li className="flex align-items-center mb-3">
@@ -196,12 +224,30 @@ const TableTransactionRecord = ({ params }: { params: Promise<{ slug: string; id
                                 <span>{dayjs().format('DD MMMM YYYY')}</span>
                             </li>
                         </ul>
-                        <hr className="mb-3 mx-0 border-top-1 border-bottom-none border-300 mt-auto" />
-                        <Button label="Pendataan Baru" className="p-3 w-full mt-auto" severity={category === 'saving' ? 'success' : 'warning'} />
+                        {!isShowForm && (
+                            <>
+                                <hr className="mb-3 mx-0 border-top-1 border-bottom-none border-300 mt-auto" />
+                                <div className="flex flex-wrap gap-2">
+                                    {category === 'saving' ? (
+                                        <>
+                                            <Button label="Simpanan Pokok" className="p-3 mt-auto" onClick={() => toggleForm('pokok')} />
+                                            <Button label="Simpanan Wajib" className="p-3 mt-auto" severity="info" onClick={() => toggleForm('wajib')} />
+                                            <Button label="Tabungan" className="p-3 mt-auto" severity="success" onClick={() => toggleForm('tabungan')} />
+                                            <Button label="Penarikan Simpanan" className="p-3 mt-auto" severity="warning" onClick={() => toggleForm('tarik')} />
+                                        </>
+                                    ) : (
+                                        <>
+                                            <Button label="Bayar Cicilan" className="p-3 mt-auto" severity="help" onClick={() => toggleForm('cicilan')} />
+                                            <Button label="Ambil Pinjaman" className="p-3 mt-auto" severity="danger" onClick={() => toggleForm('pinjaman')} />
+                                        </>
+                                    )}
+                                </div>
+                            </>
+                        )}
                     </div>
                 </div>
             </div>
-            {chartData && chartOptions && (
+            {!isShowForm && chartData && chartOptions && (
                 <div className="col-12 lg:col-6">
                     <div className="card">
                         <h5>Perkembangan</h5>
@@ -210,32 +256,94 @@ const TableTransactionRecord = ({ params }: { params: Promise<{ slug: string; id
                 </div>
             )}
 
-            <div className="col-12">
-                <div className="card">
-                    <h5>Data Transaksi {infoCategories[category]}</h5>
-                    <DataTable
-                        className="p-datatable-gridlines"
-                        header={renderHeader}
-                        loading={loading}
-                        filters={filters}
-                        value={list}
-                        rows={10}
-                        dataKey="id"
-                        filterDisplay="menu"
-                        emptyMessage="Tidak ditemukan data anggota!"
-                        paginator
-                        showGridlines
-                        stripedRows
-                        scrollable
-                    >
-                        <Column field="date" header="Tanggal" body={dateBodyTemplate} />
-                        <Column field="type" header="Jenis" body={typeBodyTemplate} />
-                        <Column field="amount" header="Nominal" body={amountBodyTemplate} />
-                        <Column field="count" header={category === 'saving' ? 'Saldo' : 'Sisa'} body={countBodyTemplate} />
-                        <Column header="" body={editBodyTemplate} className="filter-action-button" />
-                    </DataTable>
+            {!isShowForm && (
+                <div className="col-12">
+                    <div className="card">
+                        <h5>Data Transaksi {infoCategories[category]}</h5>
+                        <DataTable
+                            className="p-datatable-gridlines"
+                            header={renderHeader}
+                            loading={loading}
+                            filters={filters}
+                            value={list}
+                            rows={10}
+                            dataKey="id"
+                            filterDisplay="menu"
+                            emptyMessage="Tidak ditemukan data anggota!"
+                            paginator
+                            showGridlines
+                            stripedRows
+                            scrollable
+                        >
+                            <Column field="date" header="Tanggal" body={dateBodyTemplate} />
+                            <Column field="type" header="Jenis" body={typeBodyTemplate} />
+                            <Column field="amount" header="Nominal" body={amountBodyTemplate} />
+                            <Column field="count" header={category === 'saving' ? 'Saldo' : 'Sisa'} body={countBodyTemplate} />
+                            <Column header="" body={editBodyTemplate} className="filter-action-button" />
+                        </DataTable>
+                    </div>
                 </div>
-            </div>
+            )}
+
+            {isShowForm && (
+                <div className="col-12">
+                    <div className="card">
+                        <Toast ref={toast} />
+                        <h5>
+                            {payloadAction === 'baru' ? 'Tambah' : 'Ubah'} Data {mapTypes.find(({ value }) => value === payloadType)?.label}
+                        </h5>
+                        <div className="p-fluid">
+                            <div className="field">
+                                <label htmlFor="number">Tanggal</label>
+                                <Calendar showIcon showButtonBar dateFormat="dd MM yy" value={payloadDate} onChange={(e) => setPayloadDate(e.value ?? null)} />
+                            </div>
+                            <div className="field">
+                                <label htmlFor="amount">Nominal</label>
+                                <InputNumber id="amount" placeholder="Nominal" value={payloadAmount} onValueChange={(e) => setPayloadAmount(e.value || 0)} min={0} maxFractionDigits={0} mode="currency" currency="IDR" />
+                            </div>
+                            {payloadAction !== 'baru' && (
+                                <div className="field">
+                                    <label htmlFor="removal">Hapus data ini?</label> <br />
+                                    <InputSwitch id="removal" checked={isRemoval} onChange={(e) => setRemoval(e.value)} />
+                                </div>
+                            )}
+                        </div>
+                        <hr />
+                        <div className="flex justify-content-between flex-wrap">
+                            <Button label="Batal" icon="pi pi-times" severity="info" onClick={() => toggleForm()} />
+                            {!isSubmitLoad && (
+                                <Button
+                                    label="Simpan"
+                                    icon="pi pi-check"
+                                    className="form-action-button"
+                                    onClick={async () => {
+                                        setSubmitLoad(true);
+                                        const response = await fetch(`/api/cash/${category}/${customer?.id}`, {
+                                            method: 'POST',
+                                            body: JSON.stringify({ action: payloadAction, type: payloadType, amount: payloadAmount, date: dayjs(payloadDate).format('DD-MM-YYYY'), operator: signedAdmin?.phone, removal: isRemoval ?? false }),
+                                            headers: { 'Content-Type': 'application/json' }
+                                        });
+                                        const result = await response.json();
+                                        setSubmitLoad(false);
+
+                                        if (result?.saved) {
+                                            toggleForm();
+                                            router.refresh();
+                                        } else {
+                                            toast.current?.show({
+                                                life: 3000,
+                                                severity: 'warn',
+                                                summary: 'Gagal simpan!',
+                                                detail: 'Data tidak dapat disimpan oleh Sistem'
+                                            });
+                                        }
+                                    }}
+                                />
+                            )}
+                        </div>
+                    </div>
+                </div>
+            )}
         </div>
     );
 };
--- app/api/cash/[slug]/[account]/route.ts
+++ app/api/cash/[slug]/[account]/route.ts
@@ -1,4 +1,5 @@
 import connectDB from '@/lib/mongo';
+import admin, { IAdmin } from '@/models/admin';
 import cash, { ICash } from '@/models/cash';
 import dayjs from 'dayjs';
 import dayjsUTC from 'dayjs/plugin/utc';
@@ -33,26 +34,37 @@ export async function POST(request: NextRequest, { params }: { params: Promise<{
         const payload = await request.json();
         const { slug, account } = await params;
         const id = payload?.action === 'baru' ? uuid() : payload?.action;
+        const flagAsDelete = payload?.removal ?? false;
 
-        if (!isEmpty(payload?.type) && !isEmpty(payload?.amount)) {
-            saved = await cash
-                .findOneAndUpdate(
-                    { id },
-                    {
-                        account,
-                        category: slug,
-                        type: payload.type,
-                        amount: payload.amount,
-                        operator: payload?.operator || 'system',
-                        date: payload?.date || dayjs().format('DD-MM-YYYY'),
-                        ...(payload?.action === 'baru' ? { created: dayjs().utc().toDate() } : { updated: dayjs().utc().toDate() })
-                    },
-                    { new: true, lean: true, upsert: true }
-                )
-                .lean<ICash>();
+        if (!isEmpty(payload?.type) && payload?.amount > 0) {
+            let operator: IAdmin | null = null;
+
+            if (!isEmpty(payload?.operator)) {
+                operator = await admin.findOne({ phone: payload.operator }).lean<IAdmin>();
+            }
+
+            if (!flagAsDelete) {
+                saved = await cash
+                    .findOneAndUpdate(
+                        { id },
+                        {
+                            account,
+                            category: slug,
+                            type: payload.type,
+                            amount: payload.amount,
+                            operator: operator?.id || 'system',
+                            date: payload?.date || dayjs().format('DD-MM-YYYY'),
+                            ...(payload?.action === 'baru' ? { created: dayjs().utc().toDate() } : { updated: dayjs().utc().toDate() })
+                        },
+                        { new: true, lean: true, upsert: true }
+                    )
+                    .lean<ICash>();
+            } else {
+                saved = await cash.findOneAndDelete({ id }, { lean: true }).lean<ICash>();
+            }
+
+            response = Response.json({ saved: !saved?._id ? false : true }, { status: 200 });
         }
-
-        response = Response.json({ saved: !saved?._id ? false : true }, { status: 200 });
     } catch (error) {
         const message = error instanceof Error ? error.message : 'Server error';
         response = Response.json({ error: message, saved: false }, { status: 500 });
